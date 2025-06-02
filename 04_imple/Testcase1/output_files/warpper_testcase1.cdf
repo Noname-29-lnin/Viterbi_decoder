7694719c3ea8e696bca6074c76a77a96483da9a1
My name is Noname <dong.nguyendai29k22@hcmut.edu.vn>
2025-06-02 15:33:43 +0700

    update Testcase1

---
+++ 04_imple/Testcase1/output_files/warpper_testcase1.cdf
@@ -0,0 +1,13 @@
+/* Quartus II 64-Bit Version 13.0.1 Build 232 06/12/2013 Service Pack 1 SJ Web Edition */
+JedecChain;
+	FileRevision(JESD32A);
+	DefaultMfr(6E);
+
+	P ActionCode(Cfg)
+		Device PartName(EP2C35F672) Path("C:/Document_forme/project_tiny/Viterbi_decoder/04_imple/Testcase1/output_files/") File("warpper_testcase1.sof") MfrSpec(OpMask(1));
+
+ChainEnd;
+
+AlteraBegin;
+	ChainType(JTAG);
+AlteraEnd;
--- 04_imple/Testcase1/warpper_testcase1.sv
+++ 04_imple/Testcase1/warpper_testcase1.sv
@@ -1,11 +1,26 @@
 module warpper_testcase1(
     input logic         CLOCK_50,
     input logic [17:0]  SW      ,
+	 input logic [3:0]   KEY	  ,
     output logic [17:0] LEDR    ,
     output logic [6:0]  LEDG    ,
     output logic [6:0]  HEX0    ,
     output logic [6:0]  HEX1    
 );
+
+parameter SIZE_COUNT = 32;
+parameter CLOCK_DIV = 500000;
+logic [SIZE_COUNT-1:0] count;
+logic w_clk;
+always_ff @( posedge CLOCK_50) begin
+	count <= (count == CLOCK_DIV) ? '0 : count + 1'b1;
+end
+always_ff @( posedge CLOCK_50) begin
+	w_clk <= (count == CLOCK_DIV) ? ~w_clk : w_clk;
+end
+
+logic w_key;
+assign w_key = ~KEY[0];
 
 Testcase1 #(
     .SIZE_DATA_IN  (16    ),
@@ -14,17 +29,17 @@ Testcase1 #(
     .SIZE_SIPO     (1     ),
     .SIZE_7SEG     (7     )
 ) uut (
-    .i_clk          (CLOCK_50)  ,
+    .i_clk          (w_clk)  ,
     .i_rst_n        (SW[17])    ,
-    .i_start        (SW[16])    ,
+    .i_start        (w_key)    ,
     .i_data         (SW[15:0])  ,
 	 
     .o_HEX_0        (HEX0)      ,
     .o_HEX_1        (HEX1)      ,
-    .o_done_PISO    (LEDG[0])   ,
-    .o_done_SIPO    (LEDG[1])   
+    .o_done_PISO    (LEDG[1])   ,
+    .o_done_SIPO    (LEDG[2])   
 );
 
 assign LEDR[17:0] = SW[17:0];
-
+assign LEDG[0] = w_key;
 endmodule
